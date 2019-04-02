e4213f772178791a5d5bd184453f300d14426716
Gorm <graham@hough.io>
2019-04-02 14:14:51 -0400

    updated snyk to run in a mono-style

--- snyk/README.md
+++ snyk/README.md
@@ -3,8 +3,8 @@ Knotel's github action to run Snyk tests and output to the PR in a mono-repo.
 
 ## How to Use
 
-1. Add Action
-2. Enter `knotel/actions/snyk@master`
+1. Add the `changes` action to a workflow
+2. add this action to your workflow `knotel/actions/snyk@master`
 3. Add Secret named `SNYK_TOKEN`
 4. Go to your [Snyk Account Settings](https://app.snyk.io/account) and get your API token value
 5. Enter the value of the token you copied from Snyk as the secret in the GitHub Action
--- snyk/entrypoint.sh
+++ snyk/entrypoint.sh
@@ -1,5 +1,31 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 set -eu
 
-sh -c "snyk $*"
+FILE="~/services.json"
+
+if [ ! -f ${FILE} ]; then
+  echo "Services File does not exist."
+  exit 1
+else
+  CHANGES=($(cat $FILE | jq -r '@sh'))
+  for service in ${CHANGES[@]}; do
+    #if the service starts with a dot, don't run snyk tests
+    if [[ ${service:1:1} == "." ]]; then
+      echo "Skipping Running Snyk in $service"
+      echo
+    else
+      cd ~/
+      echo "Running Snyk for Service: $service"
+      cd ${service:1:${#service}-2}
+      echo "Running Yarn Setup!"
+      yarn
+      echo
+      echo "Running Snyk Test!"
+      snyk test
+      echo
+    fi
+  done
+fi
+
+
