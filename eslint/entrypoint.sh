aa7f7edc766a8bafd88de712955182728df17ad5
Gorm <graham@hough.io>
2019-04-08 10:35:41 -0400

    point to jq directly

--- eslint/entrypoint.sh
+++ eslint/entrypoint.sh
@@ -11,7 +11,7 @@ if [[ ! -f ~/services.json ]]; then
   echo "You will need to install dependencies before running this action."
   exit 1
 else
-  CHANGES=`cat ~/services.json | jq -r '@sh'`
+  CHANGES=`cat ~/services.json | /usr/bin/jq -r '@sh'`
   for service in ${CHANGES[@]}; do
     #if the service starts with a dot, don't run eslint tests
     if [[ ${service:1:1} == "." ]]; then
