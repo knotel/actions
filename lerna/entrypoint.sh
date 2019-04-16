91c27b32f39817f5666ec7cb0cc798bcaa5478f1
Gorm <graham@hough.io>
2019-04-16 17:22:39 -0400

    lerna action!

---
+++ lerna/entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+
+#slack chat send \
+#  --actions ${actions} \
+#  --author ${author} \
+#  --author-icon ${author_icon} \
+#  --author-link ${author-link} \
+#  --channel ${channel} \
+#  --color ${color} \
+#  --fields ${fields} \
+#  --footer ${footer} \
+#  --footer-icon ${footer-icon} \
+#  --image '${image}' \
+#  --pretext '${pretext}' \
+#  --text '${slack-text}' \
+#  --time ${time} \
+#  --title ${title} \
+#  --title-link ${title-link}
+
+# ${title-link}  = 'https://github.com/knotel/actions/lerna'
+# ${fields}      = '{"title": "Environment", "value": "snapshot", "short": true}'
+# ${channel}     = '#channel'
+# ${author-link} = 'https://github.com/knotel/actions/lerna'
+# ${fields}      = '{"title": "Environment", "value": "snapshot", "short": true}'
+# ${author-link} = 'https://github.com/rockymadden/slack-cli'
+# ${author-icon} = 'https://assets-cdn.github.com/images/modules/logos_page/Octocat.png'
+# ${footer-icon} = 'https://assets-cdn.github.com/images/modules/logos_page/Octocat.png'
+# ${actions}     = '{"type": "button", "style": "primary", "text": "See results", "url": "http://example.com"}'
+# ${image}       = "https://assets-cdn.github.com/images/modules/logos_page/Octocat.png"
+
+if [[ $1 == "slack_changed" ]]; then
+  cd /github/workspace
+  lerna changed -la | /bin/slack chat send --channel ${channel} --pretext ${pretext} --color ${color}
+else
+  cd /github/workspace
+  lerna $*
+fi
