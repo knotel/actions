ea8a9aa0fd1a51ee98513c82fabdc02ef9a688c6
Gorm <graham@hough.io>
2019-04-18 16:06:37 -0400

    cd inside ticks

--- changes-notify/entrypoint.sh
+++ changes-notify/entrypoint.sh
@@ -45,7 +45,7 @@ else
   BRANCH=$(cd /github/workspace && git rev-parse --abbrev-ref HEAD)
   COMMIT=$(cd /github/workspace && git log -n 1 --pretty=format:%H)
   ORG=$(cd /github/workspace && git config --get remote.origin.url | sed -e "s/.*github.com.\(.*\)\/\(.*\)/\1/")
-  REPO=$(cd /github/workspace && basename `git rev-parse --show-toplevel`)
+  REPO=$(basename `cd /github/workspace && git rev-parse --show-toplevel`)
   REMOTE=$(cd /github/workspace && git config --get remote.origin.url)
   FILE_URL="https://github.com/${ORG}/${REPO}/tree/${BRANCH}/${1}"
   COMMIT_URL="https://github.com/${ORG}/${REPO}/commit/${COMMIT}"
