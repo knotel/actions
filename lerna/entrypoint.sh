2026ca9438f0314afd7a1bba77541f2be62903fb
Gorm <graham@hough.io>
2019-05-31 13:38:30 -0400

    let's change the remote url to be ssh based

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -12,6 +12,37 @@ if [ $(git cat-file -p $(git rev-parse HEAD) | grep parent | wc -l) = 1 ]; then
   echo "Not a merge commit... Pulling latest."
   #or do a git pull?
   cd /github/workspace
+  REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+  if [ -z "$REPO_URL" ]; then
+    echo "-- ERROR:  Could not identify Repo url."
+    echo "   It is possible this repo is already using SSH instead of HTTPS."
+    exit
+  fi
+
+  USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
+  if [ -z "$USER" ]; then
+    echo "-- ERROR:  Could not identify User."
+    exit
+  fi
+
+  REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
+  if [ -z "$REPO" ]; then
+    echo "-- ERROR:  Could not identify Repo."
+    exit
+  fi
+
+  NEW_URL="git@github.com:$USER/$REPO.git"
+  echo "Changing repo url from "
+  echo "  '$REPO_URL'"
+  echo "      to "
+  echo "  '$NEW_URL'"
+  echo ""
+
+  CHANGE_CMD="git remote set-url origin $NEW_URL"
+  `$CHANGE_CMD`
+
+  echo "Success"
+
   git pull
   if [ $(git log -1 --pretty=%s) == "Publish" ]; then
     echo "last commit was publish"
