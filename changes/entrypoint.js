bc89d85c42be7df71c22f6d5da49cc4a8efd45a1
Gorm <graham@hough.io>
2019-06-04 12:18:20 -0400

    removing one of these, need folder/service to be service.json

--- changes/entrypoint.js
+++ changes/entrypoint.js
@@ -26,13 +26,6 @@ commits.forEach(commit => {
     }
     for (let i = 0 , len = commit.removed.length; i < len; i++) {
       let path_segments = commit.removed[i].split('/')
-      const service_name = path_segments[1]
-      path_segments.shift()
-      const service_file_path = path_segments.join('/')
-      tmp_services.push(service_name);
-    }
-    for (let i = 0 , len = commit.modified.length; i < len; i++) {
-      let path_segments = commit.modified[i].split('/')
       const service_name = path_segments[0]
       path_segments.shift()
       const service_file_path = path_segments.join('/')
