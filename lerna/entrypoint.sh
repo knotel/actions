96bc8db0fb12358f1064a2384374136661ed30ac
Willy Guggenheim <uptimepro@gmail.com>
2019-06-17 12:57:50 -0700

    fixing git pull in learna step

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -20,6 +20,18 @@ cat $HOME/.ssh/id_rsa | tail -c 9
 
 cd /github/workspace
 
+USER=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
+if [ -z "$USER" ]; then
+  echo "-- ERROR:  Could not identify User."
+  exit
+fi
+
+REPO=`echo $REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
+if [ -z "$REPO" ]; then
+  echo "-- ERROR:  Could not identify Repo."
+  exit
+fi
+
 NEW_URL="git@github.com:$USER/$REPO.git"
 git remote set-url origin $NEW_URL
 
