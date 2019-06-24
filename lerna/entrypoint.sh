03feb08e8acfececb070000b56dc1d0d1e7a93de
Gorm <graham@hough.io>
2019-06-24 15:11:19 -0400

    add http

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -7,7 +7,7 @@ echo "HOME is ${HOME}"
 if [ -n "$NPM_TOKEN" ]; then
   # Respect NPM_CONFIG_USERCONFIG if it is provided, default to $HOME/.npmrc
   NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG-"$HOME/.npmrc"}"
-  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
+  NPM_REGISTRY_URL="${NPM_REGISTRY_URL-http://registry.npmjs.org}"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
   printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
