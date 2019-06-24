13394ec9493a4c6ff2cde60ffbf95d18e4163bb3
Gorm <graham@hough.io>
2019-06-24 14:39:27 -0400

    second set of backticks

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -79,7 +79,7 @@ if [ $(git cat-file -p $(git rev-parse HEAD) | grep parent | wc -l) = 1 ]; then
       echo "Changed:"
       cat ~/changed.json
       LERNA_CHANGED="\`\`\`"
-      LERNA_CHANGED=$(cd /github/workspace && lerna changed -la)
+      LERNA_CHANGED+=$(cd /github/workspace && lerna changed -la)
       LERNA_CHANGED+="\`\`\`"
       PRETEXT="The following packages have had a minor version bump."
       /bin/slack chat send \
