8cae189d0d803ad24a700fc6d6ca532a5f4f1327
Willy Guggenheim <WillyGuggenheim@users.noreply.github.com>
2019-07-22 14:04:26 -0700

    test

--- yarn/entrypoint.sh
+++ yarn/entrypoint.sh
@@ -8,7 +8,7 @@ if [ -n "$NPM_TOKEN" ]; then
   NPM_REGISTRY_URL="${NPM_REGISTRY_URL-registry.npmjs.org}"
 
   # Allow registry.npmjs.org to be overridden with an environment variable
-  printf "//%s/:_authToken=%s\\nregistry=%s" "$NPM_REGISTRY_URL" "$NPM_TOKEN" "$NPM_REGISTRY_URL" > "$NPM_CONFIG_USERCONFIG"
+  printf "//%s/:_authToken=%s" "$NPM_REGISTRY_URL" "$NPM_TOKEN" > "$NPM_CONFIG_USERCONFIG"
   chmod 0600 "$NPM_CONFIG_USERCONFIG"
 fi
 
