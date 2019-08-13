89c6754e77795fdc36bdb966c47e19c1162e859d
Gorm <graham@hough.io>
2019-08-13 13:40:00 -0400

    more feedback

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -48,6 +48,9 @@ REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]
 if [ -z "$REPO_URL" ]; then
   echo "-- ERROR:  Could not identify Repo url."
   echo "   It is possible this repo is already using SSH instead of HTTPS."
+  echo "I was setting REPO_URL to:"
+  echo $(git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p')
+  echo ""
   exit
 fi
 
