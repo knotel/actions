e37cfe90121ea03f5b620663f1eba4255b84a0d8
Willy Guggenheim <WillyGuggenheim@users.noreply.github.com>
2019-08-16 11:23:21 -0700

    fix edge case when 2 deploys merge at the same

--- lerna/entrypoint.sh
+++ lerna/entrypoint.sh
@@ -204,6 +204,9 @@ else
   /bin/slack file upload --file ${GITHUB_WORKSPACE}/diff.patch --filetype patch --channels '#deploys' --comment "${DIFF_COMMENT}" --title 'Patch Incoming!'
 
   #Run the publish command and save the output into a logfile
+  git checkout master
+  git pull --rebas  git pull --rebase -fe -f
+  git pull origin master -f
   lerna publish --no-verify-access --from-package minor --yes > ${GITHUB_WORKSPACE}/publish.log
   PUBLISH_COMMENT="Here is the logfile for the last publish:"
   /bin/slack file upload --file ${GITHUB_WORKSPACE}/publish.log --filetype log --channels '#deploys' --comment "${PUBLISH_COMMENT}" --title 'Log Incoming!'
