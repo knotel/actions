21fd903af3167d0610b1d6ffd62ad225cf913bf9
Gorm <graham@hough.io>
2019-10-17 11:23:47 -0400

    dont' declare github twice

--- changes/entrypoint.js
+++ changes/entrypoint.js
@@ -1,7 +1,6 @@
 const github = require('@actions/github')
 
 const token = process.env.GITHUB_TOKEN;
-const github = require('@actions/github');
 const context = new github.GitHub(token);
 //context here is example.json
 const fs = require('fs');
